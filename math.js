afa08b244465944479e403d10101d4c88c87a42e
Alviura <alviura@outlook.com>
2023-09-03 19:06:57 +0300

    Basic JS Arithmetics

---
+++ math.js
@@ -0,0 +1,33 @@
+const num1 = 100;
+const num2 = 200;
+let sum;
+
+//Simple Arithmetics
+sum = num1 + num2;
+console.log(sum);
+sum = num1 - num2;
+console.log(sum);
+sum = num1 * num2;
+console.log(sum);
+sum = num1 / num2;
+console.log(sum);
+sum = num1 % num2;
+console.log(sum);
+
+//Math Object - has methods and Atributes
+val = Math.PI;
+console.log(val);
+val = Math.E;
+console.log(val);
+val = Math.round(2.4);
+console.log(val);
+val = Math.ceil(2.4);
+console.log(val)
+val = Math.floor(2.8);
+console.log(val)
+val = Math.sqrt(64);
+console.log(val)
+val = Math.abs(-3);
+console.log(val)
+val = Math.pow(8,2);
+console.log(val)
