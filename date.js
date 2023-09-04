2bbe3534b6dc14d926d74f93cf7cfd58b432c4a0
Alviura <alviura@outlook.com>
2023-09-04 23:45:04 +0300

    Date & Time

---
+++ date.js
@@ -0,0 +1,10 @@
+const today = new Date()
+
+let birthday 
+birthday = new Date('7-11-2003 11:25:00')
+birthday = new Date('November 7 2003')
+birthday = new DragEvent('10/7/2003')
+
+let val = birthday
+console.log(val)
+
