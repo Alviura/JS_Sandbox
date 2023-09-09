0143878f0689ee9321a9e8a86e769be3467ac540
Alviura <alviura@outlook.com>
2023-09-09 22:56:18 +0300

    Window  methods & Properties

---
+++ window.js
@@ -0,0 +1,16 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+alert('Hello World')
+
+// Prompt -> takes an input
+const input = prompt()
+alert(input)
+
+// Confirm -> often used to notify of what is about to do
+if(confirm('Are you sure')){
+    console.log('Yes')
+}
+else {
+    console.log('No')
+}
